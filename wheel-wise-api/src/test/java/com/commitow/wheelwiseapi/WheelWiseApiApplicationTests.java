cbe8ee38906f28737165eda1300b40413dc36f49
Joanna Sokolowska <joanna_s@op.pl>
2024-03-01 19:28:56 +0100

    Initial setup for backend part

---
+++ wheel-wise-api/src/test/java/com/commitow/wheelwiseapi/WheelWiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commitow.wheelwiseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelWiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
