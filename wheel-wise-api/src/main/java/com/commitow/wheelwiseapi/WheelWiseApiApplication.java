7b5eb96c69473c47a51ff1c21e27abf0c730af14
Joanna Sokołowska <83209613+jasokolowska@users.noreply.github.com>
2024-03-02 07:05:36 +0100

    Initial setup for backend part (#1)

---
+++ wheel-wise-api/src/main/java/com/commitow/wheelwiseapi/WheelWiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.commitow.wheelwiseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelWiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelWiseApiApplication.class, args);
+	}
+
+}
