0a2d0106ec2a99d6cef7e43f874b04b1f3872ede
Joanna Sokolowska <joanna_s@op.pl>
2024-03-12 21:19:35 +0100

    [F] Event form - adjustment

--- wheel-wise-client/src/app/events/event-form/event-form.component.ts
+++ wheel-wise-client/src/app/events/event-form/event-form.component.ts
@@ -1,11 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {MapComponent} from "../eventdetails/map/map.component";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {DropdownModule} from "primeng/dropdown";
+import {CalendarModule} from "primeng/calendar";
 
 @Component({
   selector: 'app-event-form',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    MapComponent,
+    InputTextareaModule,
+    DropdownModule,
+    CalendarModule
   ],
   templateUrl: './event-form.component.html',
   styleUrl: './event-form.component.scss'
