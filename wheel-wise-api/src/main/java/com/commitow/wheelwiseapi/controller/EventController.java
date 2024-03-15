6722ae6fe5d73efb53847b088ab608cf057505af
Joanna Sokolowska <joanna_s@op.pl>
2024-03-15 20:25:28 +0100

    [B] Openapi, Events Endpoints

--- wheel-wise-api/src/main/java/com/commitow/wheelwiseapi/controller/EventController.java
+++ wheel-wise-api/src/main/java/com/commitow/wheelwiseapi/controller/EventController.java
@@ -1,32 +1,36 @@
 package com.commitow.wheelwiseapi.controller;
 
 import com.commitow.wheelwiseapi.openapi.api.EventsApi;
-import com.commitow.wheelwiseapi.openapi.model.EventDtoDto;
+import com.commitow.wheelwiseapi.openapi.model.EventDto;
 import com.commitow.wheelwiseapi.openapi.model.RegistrationRequestDto;
 import com.commitow.wheelwiseapi.service.EventService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @AllArgsConstructor
+@RestController
+@RequestMapping(value ="/api")
 public class EventController implements EventsApi {
 
     private final EventService eventService;
 
     @Override
-    public ResponseEntity<Void> createEvent(EventDtoDto eventDtoDto) {
-        eventService.createNewEvent(eventDtoDto);
+    public ResponseEntity<Void> createEvent(EventDto eventDto) {
+        eventService.createNewEvent(eventDto);
         return ResponseEntity.accepted().build();
     }
 
     @Override
-    public ResponseEntity<List<EventDtoDto>> getAllEvents() {
+    public ResponseEntity<List<EventDto>> getAllEvents() {
         return ResponseEntity.ok(eventService.getAllEvents());
     }
 
     @Override
-    public ResponseEntity<EventDtoDto> getEvent(Integer eventId) {
+    public ResponseEntity<EventDto> getEvent(Integer eventId) {
         return ResponseEntity.ok(eventService.getEvent(eventId));
     }
 
--- wheel-wise-api/src/main/java/com/commitow/wheelwiseapi/service/EventService.java
+++ wheel-wise-api/src/main/java/com/commitow/wheelwiseapi/service/EventService.java
@@ -1,26 +1,33 @@
 package com.commitow.wheelwiseapi.service;
 
-import com.commitow.wheelwiseapi.openapi.model.EventDtoDto;
+import com.commitow.wheelwiseapi.openapi.model.EventDto;
 import com.commitow.wheelwiseapi.openapi.model.RegistrationRequestDto;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class EventService {
 
 
-    public void createNewEvent(EventDtoDto eventDtoDto) {
+    public void createNewEvent(EventDto eventDto) {
     }
 
-    public List<EventDtoDto> getAllEvents() {
+    public List<EventDto> getAllEvents() {
         return null;
     }
 
-    public EventDtoDto getEvent(Integer eventId) {
-        EventDtoDto eventDtoDto = new EventDtoDto();
-
-        return null;
+    public EventDto getEvent(Integer eventId) {
+        EventDto eventDto = new EventDto();
+        eventDto.setEventId(1);
+        eventDto.eventType("Training");
+        eventDto.setDescription("Short and intensive road training around Wrocław");
+        eventDto.setDifficultyLevel("medium");
+        eventDto.setRouteId(1);
+        eventDto.setStartDatetime(LocalDateTime.now());
+        eventDto.setEndDatetime(LocalDateTime.now());
+        return eventDto;
     }
 
     public void registerForEvent(Integer eventId, RegistrationRequestDto registrationRequestDto) {
